acef122a6d89e2730e2619920aaa0dedfe92fc7f
naiaralandeta <naiaralandetagonz@gmail.com>
2022-08-25 19:51:12 +0100

    Admin page

--- src/components/admin/medicine.js
+++ src/components/admin/medicine.js
@@ -9,10 +9,13 @@ const AdminMedicinesListFunction = () => {
     const generalURL = '/api/admin';
     const [medicineslist, setmedicines] = useState([]);
     const [modalIsOpen, setIsOpen] = React.useState(false);
-    const [search, setsearch] = useState([])
+    const [modalAddIsOpen, setAddIsOpen] = React.useState(false);
+    const [medicinetoupdate, setmedicinetoupdate] = useState([]);
 
     const handleChange = event => {
-        setsearch(event.target.value);
+        event.preventDefault();
+        const { name, value } = event.target
+        setmedicinetoupdate({ ...medicinetoupdate, [name]: value })
     };
 
     useEffect(() => {
@@ -32,14 +35,33 @@ const AdminMedicinesListFunction = () => {
             )
     }
 
-    const addMedicine = (data) => {
+    const openAddMedicine = () => {
+        setAddIsOpen(true);
+    }
 
-        fetch(generalURL + '/cart', {
+    function closeAddModal() {
+        setAddIsOpen(false);
+    }
+
+    const addMedicine = async (event) => {
+        event.preventDefault();
+        console.log(medicinetoupdate)
+        if(medicinetoupdate.discount === null){
+            medicinetoupdate.discount = 0
+        }
+        medicinetoupdate.expire = new Date(medicinetoupdate.expire)
+        fetch(generalURL + '/create', {
             method: 'POST', headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(data)
-        })
+            body: JSON.stringify(medicinetoupdate)
+        }).then(response => response.json())
+        .then(
+            () => {
+                closeAddModal()
+                getmedicines()
+            }
+        );
     }
 
     const deleteMedicine = (data) => {
@@ -61,20 +83,39 @@ const AdminMedicinesListFunction = () => {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        await fetch(generalURL + '/update?id=', {
+        await fetch(generalURL + '/update?id=' + medicinetoupdate.id, {
             method: 'PUT',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify()
-        });
-        closeModal()
+            body: JSON.stringify(medicinetoupdate)
+        }).then(response => response.json())
+            .then(
+                () => {
+                    closeModal()
+                    getmedicines()
+                }
+            );
+
     }
 
-    const updateMedicine = () => {
+    const updateMedicine = (data) => {
+        setmedicinetoupdate(data)
         openModal()
     }
+
+    const expireMedicines = () => {
+
+        fetch(generalURL + '/all', {
+            method: 'DELETE'
+        }).then(
+            () => {
+                getmedicines()
+            }
+        )
+    }
+
 
     if (!medicineslist) return (
         <div>
@@ -148,7 +189,96 @@ const AdminMedicinesListFunction = () => {
                     <div className="auth-inner-all">
                         <h2> Medicines </h2>
                         <div className="d-grid">
-                            <button onClick={() => addMedicine()}>Add medicine</button>
+                            <button onClick={() => openAddMedicine()}>Add medicine</button>
+                        </div>
+
+                        <Modal isOpen={modalAddIsOpen} onRequestClose={closeAddModal}>
+                            <form onSubmit={addMedicine}>
+                                <h3>Insert data</h3><br />
+                                <div className="mb-3">
+                                    <label>Name</label>
+                                    <input type="text" name="name"
+                                        onChange={handleChange}
+                                        className="form-control"
+                                        placeholder="Insert medicine name" />
+                                </div>
+                                <div className="mb-3">
+                                    <label>Company</label>
+                                    <input type="text" name="company"
+                                        onChange={handleChange}
+                                        className="form-control"
+                                        placeholder="Insert company name" />
+                                </div>
+                                <div className="mb-3">
+                                    <label>Price</label>
+                                    <input
+                                        type="number" name="price"
+                                        onChange={handleChange}
+                                        className="form-control"
+                                        placeholder="Insert price"
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label>Quantity</label>
+                                    <input
+                                        type="number" name="quantity"
+                                        onChange={handleChange}
+                                        className="form-control"
+                                        placeholder="Insert quantity"
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label>Uses</label>
+                                    <input type="text" name="uses"
+                                        onChange={handleChange}
+                                        className="form-control"
+                                        placeholder="Insert uses description" />
+                                </div>
+                                <div className="mb-3">
+                                    <label>Disease</label>
+                                    <input type="text" name="disease"
+                                        onChange={handleChange}
+                                        className="form-control"
+                                        placeholder="Insert disease description" />
+                                </div>
+                                <div className="mb-3">
+                                    <label>Date of expire</label>
+                                    <input
+                                        type="date" name="expire"
+                                        onChange={handleChange}
+                                        className="form-control"
+                                        placeholder="Insert a date"
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label>Discount</label>
+                                    <input
+                                        type="number" name="discount" value={0}
+                                        onChange={handleChange}
+                                        className="form-control"
+                                        placeholder="Discount"
+                                    />
+                                </div>
+                                <div className="flex-row">
+                                    <div className="flex-col">
+                                        <button type="submit" className="btn btn-primary">
+                                            Add
+                                        </button>
+                                    </div>
+                                    <div className="flex-col">
+                                        <button className="btn btn-primary" onClick={closeAddModal}>
+                                            Cancel
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </Modal>
+
+                        <div className="d-grid">
+                            <button onClick={() => expireMedicines()}>Delete expired medicines</button>
+                        </div>
+                        <div className="d-grid">
+                            <button onClick={() => addMedicine()}>Delete no demand medicines</button>
                         </div>
                         <table className="table">
                             <thead>
@@ -180,7 +310,7 @@ const AdminMedicinesListFunction = () => {
                                         <td> {medicine.quantity} </td>
                                         <td>
                                             <div className="d-grid">
-                                                <button onClick={updateMedicine}>Update</button>
+                                                <button onClick={() => updateMedicine(medicine)}>Update</button>
                                             </div>
                                             <div className="d-grid">
                                                 <button onClick={() => deleteMedicine(medicine)}>Delete</button>
@@ -191,89 +321,59 @@ const AdminMedicinesListFunction = () => {
                             </tbody>
                         </table>
                         <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
-                                <form onSubmit={handleSubmit}>
-                                    <h3>Update informartion</h3>
-                                    <div className="mb-3">
-                                        <label>Reference</label>
-                                        <input
-                                            name="id" value={medicineslist.id || ''}
-                                            className="form-control"
-                                            placeholder="Username"
-                                        />
+                            <form onSubmit={handleSubmit}>
+                                <h3>Update informartion</h3><br />
+                                <span>*Note: You can only update the following fields of a drug</span><br /><br />
+                                <div className="mb-3">
+                                    <label>Company</label>
+                                    <input type="text" name="company" value={medicinetoupdate.company || ''}
+                                        onChange={handleChange}
+                                        className="form-control"
+                                        placeholder="Company" />
+                                </div>
+                                <div className="mb-3">
+                                    <label>Price</label>
+                                    <input
+                                        type="number" name="price" value={medicinetoupdate.price || ''}
+                                        onChange={handleChange}
+                                        className="form-control"
+                                        placeholder="Price"
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label>Quantity</label>
+                                    <input
+                                        type="number" name="quantity" value={medicinetoupdate.quantity || ''}
+                                        onChange={handleChange}
+                                        className="form-control"
+                                        placeholder="Quantity"
+                                    />
+                                </div>
+
+                                <div className="mb-3">
+                                    <label>Discount</label>
+                                    <input
+                                        type="number" name="discount" value={medicinetoupdate.discount || 0}
+                                        onChange={handleChange}
+                                        className="form-control"
+                                        placeholder="Discount"
+                                    />
+                                </div>
+
+                                <div className="flex-row">
+                                    <div className="flex-col">
+                                        <button type="submit" className="btn btn-primary">
+                                            Update
+                                        </button>
                                     </div>
-                                    <div className="mb-3">
-                                        <label>First name</label>
-                                        <input
-                                            type="text" name="firstname" value={ ''}
-                                            onChange={handleChange}
-                                            className="form-control"
-                                            placeholder="First name"
-                                        />
+                                    <div className="flex-col">
+                                        <button className="btn btn-primary" onClick={closeModal}>
+                                            Cancel
+                                        </button>
                                     </div>
-                                    <div className="mb-3">
-                                        <label>Last name</label>
-                                        <input type="text" name="lastname" value={ ''}
-                                            onChange={handleChange}
-                                            className="form-control" placeholder="Last name" />
-                                    </div>
-                                    <div className="mb-3">
-                                        <label>Email address</label>
-                                        <input
-                                            type="email" name="email" value={ ''}
-                                            className="form-control"
-                                            placeholder="Enter email"
-                                        />
-                                    </div>
-                                    <div className="mb-3">
-                                        <label>Password</label>
-                                        <input
-                                            type="text" name="password" value={ ''}
-                                            onChange={handleChange}
-                                            className="form-control"
-                                            placeholder="Enter password"
-                                        />
-                                    </div>
-                                    <div className="mb-3">
-                                        <label>Date of birthday</label>
-                                        <input
-                                            type="date" name="dob" value={ ''}
-                                            onChange={handleChange}
-                                            className="form-control"
-                                            placeholder="DOB"
-                                        />
-                                    </div>
-                                    <div className="mb-3">
-                                        <label>Phone</label>
-                                        <input
-                                            type="text" name="phone" value={ ''}
-                                            onChange={handleChange}
-                                            className="form-control"
-                                            placeholder="Phone"
-                                        />
-                                    </div>
-                                    <div className="mb-3">
-                                        <label>Address</label>
-                                        <input
-                                            type="text" name="address" value={ ''}
-                                            onChange={handleChange}
-                                            className="form-control"
-                                            placeholder="Address"
-                                        />
-                                    </div>
-                                    <div className="flex-row">
-                                        <div className="flex-col">
-                                            <button type="submit" className="btn btn-primary">
-                                                Update
-                                            </button>
-                                        </div>
-                                        <div className="flex-col">
-                                            <button className="btn btn-primary" onClick={closeModal}>
-                                                Cancel
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </Modal>
+                                </div>
+                            </form>
+                        </Modal>
                     </div>
                 </div>
             </div>
